d9d26cf800f622b3be4a19f70eb3d462b88a1bf5
lethargy <qaimk@rocketmail.com>
2023-12-06 15:29:26 +0000

    Creation Files

---
+++ CodeTimeTracker/CodeTimeTracker/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Data.SQLite;
+
+namespace CodeTimeTracker
+{
+    internal class Program
+    {
+        public static string connectionString = @"Data Source=..\..\..\Files\Library.db;Version=3;";
+        static void Main()
+        {
+            CreateDatabase();
+            GetUserInput();
+        }
+
+        static void CreateDatabase()
+        {
+            if (!File.Exists(@"..\..\..\Files\Library.db"))
+            {
+                SQLiteConnection.CreateFile(@"..\..\..\Files\Library.db");
+
+
+                using (SQLiteConnection connection = new SQLiteConnection(connectionString))
+                {
+                    connection.Open();
+                    SQLiteCommand tableCmd = connection.CreateCommand();
+
+                    tableCmd.CommandText =
+                        @"CREATE TABLE IF NOT EXISTS drinking_water (
+                    Id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    Date TEXT,
+                    Quantity INTEGER
+                    )";
+
+                    tableCmd.ExecuteNonQuery();
+
+                    connection.Close();
+                }
+            }
+        }
+    }
+}
